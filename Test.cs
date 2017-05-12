6e5d0e8ed0c8c9c2676696bc435e7e86a2102b7f
Victor Grigoriu <vgrigoriu@gmail.com>
2017-05-12 15:42:23 +0300

    Create csproj

--- Test.cs
+++ Test.cs
@@ -1,15 +1,5 @@
-abstract class Tree {}
-
-delegate Tree Parser(string input);
-
-delegate (Tree, string) Parser(string input);
-
-delegate (Tree, string)? Parser(string input);
-
-delegate IEnumerable<(Tree, string)> Parser(string input);
-
-delegate IEnumerable<(TTree, string)> Parser<TTree>(string input);
-
-delegate IEnumerable<(TTree, IEnumerable<char>)> Parser<TTree>(IEnumerable<char> input);
+using System.Collections.Generic;
 
 delegate IEnumerable<(TTree, IEnumerable<TToken>)> Parser<TTree, TToken>(IEnumerable<TToken> input);
+
+delegate IEnumerable<(TTree, IEnumerable<char>)> Parser<TTree>(IEnumerable<char> input);
